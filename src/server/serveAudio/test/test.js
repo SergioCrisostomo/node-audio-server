039735b17ee7062cd7449d8e6f193d7a38beae34
SergioCrisostomo <sergiosbox@gmail.com>
2020-07-21 20:38:54 +0200

    Add serve audio tests

---
+++ src/server/serveAudio/test/test.js
@@ -0,0 +1,53 @@
+const assert = require("assert");
+const path = require("path");
+const fs = require("fs").promises;
+const resetTestFolder = require("../../utils/resetTestFolder");
+const serveAudio = require("../index");
+
+const createPlaylistFolder = path.join(__dirname, "../../createPlaylist/");
+const createPlaylists = require(createPlaylistFolder + "index");
+
+const testFolder = path.join(createPlaylistFolder, "/test/");
+const { OUTPUT_MEDIA_FOLDER } = require(testFolder + "config");
+const testMediaFolder = path.join(testFolder, "/media/");
+const outputMediaFolder = path.join(testMediaFolder, OUTPUT_MEDIA_FOLDER);
+
+const getChunkData = serveAudio({
+  mediaFolder: outputMediaFolder,
+  cacheLifetime: 2,
+});
+
+describe("Serve audio", () => {
+  beforeEach(resetTestFolder);
+  afterEach(resetTestFolder);
+
+  it("Should serve the expected audio chunk", async () => {
+    const ID = "long_input_44100_000.flac";
+
+    await createPlaylists(testMediaFolder, outputMediaFolder);
+    const expected = await fs.readFile(path.join(outputMediaFolder, ID));
+    const output = await getChunkData(ID);
+    assert.equal(output.toString().length > 0, true);
+    assert.equal(output.toString(), expected.toString());
+  });
+
+  it("Should cache audio chunks", async () => {
+    const ID = "long_input_44100_000.flac";
+
+    await createPlaylists(testMediaFolder, outputMediaFolder);
+    const start = Date.now();
+    const expected = await fs.readFile(path.join(outputMediaFolder, ID));
+    const fileReadTime = Date.now() - start;
+    const output = await getChunkData(ID);
+    const firstGetCall = Date.now() - fileReadTime - start;
+    const output2 = await getChunkData(ID);
+    const secondGetCall = Date.now() - fileReadTime - firstGetCall - start;
+
+    const readTimesAreSimilar = fileReadTime / firstGetCall < 2;
+    const cacheTimeIsMuchFaster =
+      firstGetCall / Math.min(secondGetCall, 0.1) > 10; // at least 10x faster
+
+    assert.equal(readTimesAreSimilar, true);
+    assert.equal(cacheTimeIsMuchFaster, true);
+  });
+});
