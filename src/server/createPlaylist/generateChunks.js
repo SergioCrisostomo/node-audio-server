3cc36935edbb50b7b0016468dd72e2ffda1a1f1d
SergioCrisostomo <sergiosbox@gmail.com>
2020-08-04 11:17:07 +0200

    Make encryption step optional

--- src/server/createPlaylist/generateChunks.js
+++ src/server/createPlaylist/generateChunks.js
@@ -8,9 +8,24 @@ module.exports = async function generateChunks(options) {
   // options will always be the second argument...
   const runWithOptions = (fn) => (res) => fn(res, options);
 
-  return Promise.resolve()
+  console.log("Starting file processing pipeline for ", options.inputFileName);
+  let pipeline = Promise.resolve()
     .then(runWithOptions(wavToSegmentedWav))
     .then(runWithOptions(wavSegmentsToMp4))
-    .then(runWithOptions(mp4ToFragmentedMp4))
-    .then(runWithOptions(encryptFilesBento4));
+    .then(runWithOptions(mp4ToFragmentedMp4));
+
+  if (options.encryptionKeys) {
+    pipeline = pipeline.then(runWithOptions(encryptFilesBento4));
+  }
+
+  return pipeline
+    .then(() =>
+      console.log(
+        "Finished file processing pipeline for ",
+        options.inputFileName
+      )
+    )
+    .catch((err) =>
+      console.log("Pipeline problems... uncaught error inside", err)
+    );
 };
