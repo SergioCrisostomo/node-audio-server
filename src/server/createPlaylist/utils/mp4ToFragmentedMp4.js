9fe7e4b68c2207294619bc2c150f1468bd80ec99
SergioCrisostomo <sergiosbox@gmail.com>
2020-08-05 22:41:31 +0200

    Improve readability

--- src/server/createPlaylist/utils/mp4ToFragmentedMp4.js
+++ src/server/createPlaylist/utils/mp4ToFragmentedMp4.js
@@ -2,38 +2,34 @@ const path = require("path");
 const fs = require("fs").promises;
 const spawn = require("./spawn");
 
-const mp4ToFragmentedMp4 = (files) => {
+const mp4ToFragmentedMp4 = async (files) => {
   // framgment files
-  const processors = files.map((fileName) => {
-    const args = ["-dash", "10000", "-frag", "10000", "-rap", fileName];
-    return spawn(["MP4Box", args, { cwd: path.dirname(fileName) }], false);
-  });
-  return Promise.all(processors)
-    .then(() => {
-      // rename files since I'm not sure how to instruct MP4Box the output file name
-      const renames = files.map((fileName) => {
-        const [name, ext] = fileName.split(".");
-        const dashName = `${name}_dashinit.${ext}`;
-        return fs.rename(dashName, fileName);
-      });
-
-      return Promise.all(renames)
-        .catch((err) => console.log("Error renaming back files", err))
-        .then(() => {
-          // delete extra dash .mpd files
-          const deletions = files.map((fileName) => {
-            const [name] = fileName.split(".");
-            const dashMpd = `${name}_dash.mpd`;
-
-            return fs.unlink(dashMpd);
-          });
-          return Promise.all(deletions).catch((err) =>
-            console.log("Error deleting .mpd files", err)
-          );
-        })
-        .then(() => files);
+  await Promise.all(
+    files.map((fileName) => {
+      const args = ["-dash", "10000", "-frag", "10000", "-rap", fileName];
+      return spawn(["MP4Box", args, { cwd: path.dirname(fileName) }], false);
     })
-    .catch((err) => console.log("Error using mp4ToFragmentedMp4", err));
+  ).catch((err) => console.log("Error using MP4Box on", fileName, err));
+
+  // rename files since MP4Box cannot re-write same file name
+  await Promise.all(
+    files.map((fileName) => {
+      const [name, ext] = fileName.split(".");
+      const dashName = `${name}_dashinit.${ext}`;
+      return fs.rename(dashName, fileName);
+    })
+  ).catch((err) => console.log("Error renaming back files", err));
+
+  // delete extra dash .mpd files
+  await Promise.all(
+    files.map((fileName) => {
+      const [name] = fileName.split(".");
+      const dashMpd = `${name}_dash.mpd`;
+      return fs.unlink(dashMpd);
+    })
+  ).catch((err) => console.log("Error deleting .mpd files", err));
+
+  return files;
 };
 
 module.exports = mp4ToFragmentedMp4;
